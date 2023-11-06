12fc7a4cd609aa3c85c579de24462b51f1540d3b
Marie <marie.occelli13@gmail.com>
2023-11-06 17:26:33 +0100

    added toaster to Cart page

--- src/Cart/CartTotal.jsx
+++ src/Cart/CartTotal.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React from "react";
 import { Flex, Text } from "@chakra-ui/react";
 
 const CartTotal = ({ cart }) => {
-  const cartSubTotal = cart.reduce((total, item) => total + (item.price *item.qty), 0);
+  const cartSubTotal = cart.reduce(
+    (total, item) => total + item.price * item.qty,
+    0
+  );
 
   return (
     <>
-      <Flex w={'100%'} justifyContent={'flex-end'} p="1rem">
-        <Text fontSize={'2xl'} fontWeight={'700'}>Total: &#36;{cartSubTotal.toFixed(2)}</Text>
+      <Flex w={"100%"} justifyContent={"flex-end"} p="1rem">
+        <Text fontSize={"2xl"} fontWeight={"700"}>
+          Total: &#36;{cartSubTotal.toFixed(2)}
+        </Text>
       </Flex>
     </>
-  )
-}
+  );
+};
 
-export default CartTotal
+export default CartTotal;
