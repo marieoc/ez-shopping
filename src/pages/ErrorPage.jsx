a89d4e98896e00041f9f0d82bcc1bb4006cdb199
afker please <afker2020@gmail.com>
2023-11-07 10:04:17 +0100

    page error done

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,27 @@
+import { Box, Heading, Text, Center } from "@chakra-ui/react";
 import React from "react";
 
 function ErrorPage() {
-  return <div>ErrorPage</div>;
+  return (
+    <Box h={"100vh"}>
+      <Center mt={10}>
+        <Heading
+          color="yellow.400"
+          as={"h1"}
+          fontSize={"90px"}
+          size={"4xl"}
+          position="center"
+        >
+          Error 404
+        </Heading>
+      </Center>
+      <Center mt={2}>
+        <Text as={"b"} color="yellow.400" fontSize={"40px"}>
+          Page not found !
+        </Text>
+      </Center>
+    </Box>
+  );
 }
 
 export default ErrorPage;
