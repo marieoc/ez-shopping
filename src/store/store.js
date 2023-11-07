076dea3638048815c301aafe43832a820b81686f
Marie <marie.occelli13@gmail.com>
2023-11-07 09:51:08 +0100

    middleware localStorage

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,37 @@
 import { createStore } from "redux";
 import reducer from "../reducer/index";
 
-const store = createStore(reducer);
+// save to the localStorage
+function saveToLocalStorage(state) {
+    // console.log('save to localStorage');
+    try {
+        const serialisedState = JSON.stringify(state);
+        console.log(serialisedState);
+        localStorage.setItem('ez-shopping', serialisedState);
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+// load data from localStorage
+function loadFromLocalStorage() {
+    // console.log('load from localStorage');
+    try {
+        const serialisedState = localStorage.getItem('ez-shopping');
+        if (serialisedState === null) return undefined;
+        return JSON.parse(serialisedState);
+    }
+    catch (err) {
+        console.log(err);
+        return undefined;
+    }
+}
+
+// When page is loaded, get the data stored in localStorage
+const store = createStore(reducer, loadFromLocalStorage());
+
+// Each time store is changing, store the new data to localStorage 
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 export default store;
